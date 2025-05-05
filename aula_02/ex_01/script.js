ea7ad6aeec60d75c4d39c3ed990099314fbb127e
Gabriel Calixto <gfcdev@outlook.com.br>
2025-05-05 20:53:07 -0300

    Update script.js

--- aula_02/ex_01/script.js
+++ aula_02/ex_01/script.js
@@ -1,10 +1,24 @@
 
 var nome = "Gabriel";
 var idade = 21;
+//1
 
 console.log("Olá, mundo!")
-console.log("Nome:", nome)
-console.log("Idade:", idade)
 
+//2
+console.log("Nome:", nome, "\nIdade:", idade)
 
+//3
+//R: Hello
+
+//4
 console.log('Lista de compras: \n- Arroz \n- Feijão \n- Oléo')
+
+//5
+console.log('- 1 \n- 2 \n- 3 \n- 4 \n- 5')
+
+//6
+//Código está faltando "" (aspas)
+
+// 7 - b. “1 2 3 4”
+console.log("1", "2", "3", "4")
