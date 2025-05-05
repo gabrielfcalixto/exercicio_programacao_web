9bf29555c7560e3f5340c48708d0410f32982028
Gabriel Calixto <gfcdev@outlook.com.br>
2025-05-05 19:24:12 -0300

    finalizado

---
+++ aula_02/ex_01/script.js
@@ -0,0 +1,10 @@
+console.log("Olá, mundo!")
+console.log("Nome: Gabriel")
+console.log("Idade: 21")
+
+console.log("- Lista de compras")
+console.log("- Arroz")
+console.log("- Feijão")
+console.log("- Óleo")
+
+
